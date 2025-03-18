0a7c12e446fe78168387066499a6ac5f149e8617
Tobi <tobilesi@gmail.com>
2025-03-18 01:50:02 +0100

    archive mobile and contact mobile pages done

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,5 +2,18 @@ fetch('/pages/footer.html')
   .then((response) => response.text()) // Convert response to text
   .then((html) => {
     document.getElementById('footer-container').innerHTML = html
+
+    // Apply conditional logic for footer text
+    const footerText = document.getElementById('footer-text')
+    const currentPage = window.location.pathname
+
+    if (
+      currentPage.includes('archives.html') ||
+      currentPage.includes('about.html')
+    ) {
+      footerText.textContent = 'ARCOFTA'
+    } else {
+      footerText.innerHTML = 'Arc 1 <br> "From the Ground Up"'
+    }
   })
   .catch((error) => console.error('Error loading the footer:', error))
